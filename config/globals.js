3252796bbdab8b4c3e617c736feea3adb5b35689
HarryKural <harrykural333@gmail.com>
2017-03-10 21:03:12 -0500

    adding all files for Lab 5

---
+++ config/globals.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Harshit Sharma on 08-Feb-2017.
+ */
+
+// array of global variables
+module.exports = {
+    //db: 'mongodb://localhost/comp2068'};  // local mongodb
+    db: 'mongodb://HarryKural:003334287HS@ds145649.mlab.com:45649/comp2068'}; // mlab
